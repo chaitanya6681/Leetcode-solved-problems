c8ea715c3978247114fd4fc5e332f2db7ca34605
Chaitanya <mail2mchaitanya@gmail.com>
2026-07-31 14:18:28 +0530

    Time: 30 ms (97.61%), Space: 90.8 MB (29.11%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i=0,j=0,r=0;
+        while(i<players.length && j<trainers.length){
+            if(players[i]<=trainers[j]){
+                 r++;i++;j++;
+            }
+            else j++;
+        } return r;
+    }
+}
